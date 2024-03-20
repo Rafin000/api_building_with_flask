fba99fbdf29e33cf16093dc8c0f5b9a551f831a2
Rafin000 <marufulislam00000@gmail.com>
2024-03-20 15:11:54 +0600

    Modified

--- src/api/auth/views.py
+++ src/api/auth/views.py
@@ -1,15 +1,18 @@
 from flask import request
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 
 from src.api.auth.helpers import authenticate_user, generate_token
 
-auth_namespace = Namespace("users") 
+auth_namespace = Namespace("auth") 
 
+auth = auth_namespace.model('Auth', {
+    'username': fields.String(required=True),
+    'password': fields.String(required=True), 
+})
 class Login(Resource):
-
-    """Login"""
-
+    @auth_namespace.expect(auth, validate=True)
     def post(self):
+        """Login"""
         post_data = request.get_json()
         username = post_data.get('username')
         password = post_data.get('password')
